c4e1604ef9d9cce073ea89550f995b2fe49b404d
jonfresh <96211078+jonfresh@users.noreply.github.com>
2023-01-31 23:28:59 +0000

    updated colour logic and function flow

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-const defaultSize = 16
-const defaultColor = "#000000"
+const defaultSize = 64
+const defaultColor = "#f00"
 const defaultMode = 0
 
 let currentSize = defaultSize
 let currentColor = defaultColor
 let currentMode = defaultMode
 
-function setSize(newSize) {
-  gridSize(newSize)
+function setSize() {
   clearGrid()
+  updateSize()
+  gridSize(currentSize, currentSize)
 }
 
 function setColor(newColor) {
   getColor(newColor)
-  setSwatch(newColor)
+  
 }
 
 function setMode(newMode) {
@@ -22,16 +23,22 @@ function setMode(newMode) {
 }
 
 const container = document.getElementById("gridContainer");
-const colorPicker = new iro.ColorPicker('#picker');
 const eraserBtn = document.getElementById("eraserBtn");
 const clearBtn = document.getElementById("clearBtn");
 const ludicrousBtn = document.getElementById("ludicrousBtn");
 const gridSlider = document.getElementById("gridSlider");
-const gridRangeValue = document.getElementById("rangeValue");
+const colorPicker = new iro.ColorPicker('#picker', {
+  width: 250,
+  color: "#f00"
+});
 
+colorPicker.on('color:change', (color) => { setColor(color.hexString)});
+colorPicker.on('color:change', (color) => { setMode('color')});
 eraserBtn.addEventListener("click", () => { setMode('eraser')});
 clearBtn.addEventListener("click", () => { setMode('clear')});
 ludicrousBtn.addEventListener("click", () => { setMode('ludicrous')});
+gridSlider.addEventListener("change", () => { document.getElementById("rangeValue").textContent = gridSlider.value; }, false);
+gridSlider.addEventListener("change", () => { setSize()});
 
 
 function getColor(newColor) {
@@ -49,13 +56,8 @@ function getMode(newMode) {
   } else if (newMode === "clear") {
     clearGrid()
   } else {
-    let e = newMode
-    alert(e)
+    return
   }
-}
-
-function eraserTool(newMode) {
-
 }
 
 function clearGrid() {
@@ -63,33 +65,42 @@ function clearGrid() {
 }
 
 function activateBtn(newMode) {
-  Array.from(document.querySelectorAll(".button")).forEach((el) => el.classList.remove('selectedBtn'));
   if (newMode === 'eraser') {
-    document.getElementById("eraserBtn").classList.add("selectedBtn");
+    document.getElementById("eraserBtn").classList.toggle("selectedBtn");
+    document.getElementById("ludicrousBtn").classList.remove("selectedBtn");
+  } else if (newMode === 'ludicrous') {
+    document.getElementById("ludicrousBtn").classList.toggle("selectedBtn");
+    document.getElementById("eraserBtn").classList.remove("selectedBtn");
   } else {
-    return;
+    Array.from(document.querySelectorAll(".button")).forEach((el) => el.classList.remove('selectedBtn'));
   }
 }
 
+function updateSize(gridRangeValue) {
+  currentSize = document.getElementById("rangeValue").textContent = gridSlider.value;
+}
+
 function gridSize(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty('--grid-rows', rows)
+  container.style.setProperty('--grid-cols', cols)
   for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).classList = "gridItem";
+    let cell = document.createElement("div")
+    container.appendChild(cell).classList = "gridItem"
   };
 };
 
-let size = 16;
+// Set Max Grid 
+gridSize(64, 64)
+// Set Starting Grid 
+gridSize(16, 16)
 
-gridSize(size, size);
-
-
-let hoverCell = document.querySelectorAll('.gridItem');
+let hoverCell = document.querySelectorAll('.gridItem')
   for (const color of hoverCell) {
+    if (currentMode !== 'ludicrous') {
     color.addEventListener('mouseover', () => {
-      color.style.backgroundColor = currentColor;
-    });
+      color.style.backgroundColor = currentColor
+    })} else {
+    color.addEventListener('mouseover', () => {
+      color.style.backgroundColor = '#red'
+    })}
   }
-
-
